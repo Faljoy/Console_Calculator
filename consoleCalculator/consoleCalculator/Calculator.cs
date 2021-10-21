e99164dfb25b339b9d3951846aa7f6ae3fdfec9b
DESKTOP-8SKND86\Legion <Faljoy.Kodzy@i.ua>
2021-10-21 15:35:08 +0300

    add a mod and check input

--- consoleCalculator/consoleCalculator/Calculator.cs
+++ consoleCalculator/consoleCalculator/Calculator.cs
@@ -10,21 +10,49 @@ namespace consoleCalculator
         {
             return num1 + num2;
         }
+
         public static double Substract(double num1, double num2)
         {
             return num1 - num2;
         }
+
         public static double Multiply(double num1, double num2)
         {
             return num1 * num2;
         }
+
         public static double Division(double num1, double num2)
         {
             return num1 / num2;
         }
+
         public static double Sqrt(double num1)
         {
             return Math.Sqrt(num1);
+        }
+
+        public static double Divide(double x , double y)
+        {
+            while (y == 0)
+            {
+                Console.WriteLine("It`s a 0");
+                return 0;
+            }
+            return x / y;
+        }
+
+        public static double ReturnDouble()
+        {
+            string num = Console.ReadLine();
+            bool check = Double.TryParse(num, out double x);
+            if (!check)
+            {
+                Console.WriteLine("false input, try again:");
+                num = Console.ReadLine();
+                check = Double.TryParse(num, out x);
+
+            }
+            return x;
         }
 
     }
